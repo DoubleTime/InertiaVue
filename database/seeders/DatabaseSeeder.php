03384f2e4a21a5d91ed73e9fa3030241fe6abc48
Eric <erickk89@gmail.com>
2024-02-27 18:06:54 +0800

    add default seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        //Create admin
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'username' => 'admin',
+            'password' => Hash::make('12345678'),
+            'active' => true,
+        ]);
     }
 }
